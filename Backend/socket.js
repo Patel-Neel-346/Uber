8dd132f3e9202aa7eb871006413b9491cf0056ef
Patel-Neel-346 <neelpatel6340@gmail.com>
2025-03-10 14:27:22 +0530

    cors

--- Backend/socket.js
+++ Backend/socket.js
@@ -7,7 +7,7 @@ let io;
 function initializeSocket(server) {
     io = socketIo(server, {
         cors: {
-            origin: 'https://uber-frontend-brown.vercel.app',
+            origin: '*',
             methods: ['GET', 'POST'],
             credentials: true,
           },
