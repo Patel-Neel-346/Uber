508680947f39287efa5cde5c6a42480bfb5b6b03
Patel-Neel-346 <neelpatel6340@gmail.com>
2025-03-11 21:08:25 +0530

    f

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = io("https://uber-backend-omega.vercel.app/", {
+const socket = io(`${import.meta.env.VITE_BASE_URL}`, {
     transports: ["websocket"],
     reconnection: true,
     reconnectionAttempts: 5,
