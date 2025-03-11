890371de80729d01412b6c4eb467078cdd6a9a16
Patel-Neel-346 <neelpatel6340@gmail.com>
2025-03-11 21:01:54 +0530

    again

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = io(`${import.meta.env.VITE_BASE_URL}`); 
+const socket = io("https://uber-backend-omega.vercel.app"); 
 console.log(socket)
 
 const SocketProvider = ({ children }) => {
